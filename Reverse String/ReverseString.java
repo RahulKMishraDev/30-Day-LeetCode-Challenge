1a143d62b03c89107d0dc946a556e7a30ed3d27d
RahulKMishraDev <rahulkumarmishra2003@gmail.com>
2024-08-26 21:47:27 +0530

    Reverse String

---
+++ Reverse String/ReverseString.java
@@ -0,0 +1,14 @@
+
+class ReverseString {
+    public void reverseString(char[] s) {
+        int i=0;
+        int j=s.length-1;
+        while(i<=j){
+            char ch=s[i];
+            s[i]=s[j];
+            s[j]=ch;
+            i++;
+            j--;
+        }
+    }
+}
